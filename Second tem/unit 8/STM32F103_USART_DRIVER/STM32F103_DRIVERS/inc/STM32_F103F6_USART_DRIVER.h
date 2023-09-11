b3dd21b5ab278e141e3643014bef49f1d066352e
Ali Abooof <aliabooof42@gmail.com>
2023-09-11 07:00:41 +0300

    usartdriver

---
+++ Second tem/unit 8/STM32F103_USART_DRIVER/STM32F103_DRIVERS/inc/STM32_F103F6_USART_DRIVER.h
@@ -0,0 +1,139 @@
+/*
+ * usart
+ *
+ *      Author: Aliab
+ */
+
+#ifndef INCLUDE_FOLDER_STM32_F103C6_USART_DRIVER_H_
+#define INCLUDE_FOLDER_STM32_F103C6_USART_DRIVER_H_
+
+#include "STM32_F103C6_GPIO_Driver.h"
+#include "STM32_F103X6.h"
+#include "STM32_F103C6_RCC_DRIVER.h"
+
+
+//Configuration Structure
+typedef struct{
+	uint8_t 		USART_mode; // Specifies the TX/RX mode.
+			//This parameter must be set based on @ref USART_mode_define
+
+	uint32_t 		baud_rate; // This configures the USART  baud rate
+	//This parameter must be set based on @ref USART_baud_rate_define
+
+	uint8_t 		data_lenght; // Specifies the number of data bits transmitted
+								//or received in a frame.
+	//This parameter must be set based on @ref USART_data_Length_define
+
+	uint8_t 		parity;	//Specifies the parity mode.
+		//This parameter must be set based on @ref USART_Parity_define
+
+	uint8_t 		stop_bits; //Specifies the number of stop bits transmitted
+	//This parameter must be set based on @ref USART_Stop_Bits_define
+
+	uint8_t			hwFlCt;	 /* this is hwFlCt = hardware flow control
+	Specifies whether the hardware flow control mode is enabled or disabled
+	This parameter must be set based on @ref USART_HwFlCt_define*/
+
+	uint8_t 		IRQ_enable;//Enable or disable UART IRQ TX/RX
+	//This parameter must be set based on @ref USART_IRQ_Enable_define
+
+	void(* p_IRQ_callback)(void); //The C Function() which will be called once the IRQ happen
+
+}USART_config_t;
+
+
+
+//-------------------------------------------------------------------
+//==================Macros Configuration References==================
+//-------------------------------------------------------------------
+
+//@ref USART_mode
+#define USART_MODE_RX						(uint32_t)(1<<2) //Bit 2 RE: Receiver enable
+#define USART_MODE_TX						(uint32_t)(1<<3) //Bit 3 TE: Transmitter enable
+#define USART_MODE_TX_RX					(uint32_t)(1<<3 | 1<<2) //For enabling both at the same time
+
+
+//@ref USART_baud_rate
+#define USART_BAUD_RATE_2400                   2400
+#define USART_BAUD_RATE_9600                   9600
+#define USART_BAUD_RATE_19200                  19200
+#define USART_BAUD_RATE_57600                  57600
+#define USART_BAUD_RATE_115200                 115200
+#define USART_BAUD_RATE_230400                 230400
+#define USART_BAUD_RATE_460800                 460800
+#define USART_BAUD_RATE_921600                 921600
+#define USART_BAUD_RATE_2250000                2250000
+#define USART_BAUD_RATE_4500000                4500000
+
+
+//@ref USART_data_Length
+#define USART_DATA_LENGHT_8B				(uint32_t)(0) //0: 1 Start bit, 8 Data bits, n Stop bit
+#define USART_DATA_LENGHT_9B				(uint32_t)(1<<12) //1: 1 Start bit, 9 Data bits, n Stop bit
+
+
+//@ref USART_Parity
+#define USART_PARITY_NONE            	    (uint32_t)(0)
+#define USART_PARITY_EVEN              	((uint32_t)1<<10)
+#define USART_PARITY_ODD               	((uint32_t)(1<<10 | 1<<9))
+
+
+//@ref USART_Stop_Bits
+/*Bits 13:12 STOP: STOP bits
+These bits are used for programming the stop bits.
+00: 1 Stop bit
+01: 0.5 Stop bit
+10: 2 Stop bits
+11: 1.5 Stop bit*/
+#define USART_STOP_BITS_half        	    (uint32_t)(1<<12)
+#define USART_STOP_BITS_1        	        (uint32_t)(0)
+#define USART_STOP_BITS_1_half          	(uint32_t)(3<<12)
+#define USART_STOP_BITS_2           	    (uint32_t)(2<<12)
+
+
+//@ref USART_HwFlCt
+#define	USART_HWFLCT_NONE					(uint32_t)(0)
+#define	USART_HWFLCT_RTS					(uint32_t)(1<<8)
+#define	USART_HWFLCT_CTS					(uint32_t)(1<<9)
+#define	USART_HWFLCT_RTS_CTS				(uint32_t)(1<<9 | 1<<8)
+
+//@ref UART_IRQ_Enable
+//You can select two or three parameters EX.USART_IRQ_Enable_TXE |USART_IRQ_Enable_TC
+#define USART_IRQ_ENABLE_NONE                (uint32_t)(0)
+#define USART_IRQ_ENABLE_TXE                 (uint32_t)(1<<7) //Transmit data register empty
+#define USART_IRQ_ENABLE_TC                  (uint32_t)(1<<6) //Transmission complete
+#define USART_IRQ_ENABLE_RXNEIE              (uint32_t)(1<<5) //Received data ready to be read & Overrun error detected
+#define USART_IRQ_ENABLE_PE                  (uint32_t)(1<<8) //Parity error
+
+
+
+enum polling{
+	enable,
+	disable
+};
+
+
+
+//------------------------------------------------------------------------------------------
+//								APIs supported by "MCAL USART Driver"
+//------------------------------------------------------------------------------------------
+
+//Initializations APIs
+void MCAL_USART_init(USART_Typedef * USARTX,USART_config_t * config);
+void MCAL_USART_deinit(USART_Typedef * USARTX);
+
+//GPIO related functions
+void MCAL_USART_set_pins(USART_Typedef * USARTX);
+
+//Transmission functions
+void MCAL_USART_send_data(USART_Typedef * USARTX, uint16_t* data, enum polling poll);
+void MCAL_USART_recive_data(USART_Typedef * USARTX, uint16_t* data, enum polling poll);
+
+void MCAL_USART_TC_status(USART_Typedef * USARTX); //Checks if the transmission has completed or not
+
+
+
+
+
+
+
+#endif /* INCLUDE_FOLDER_STM32_F103C6_USART_DRIVER_H_ */
---
+++ Second tem/unit 8/STM32F103_USART_DRIVER/STM32F103_DRIVERS/inc/STM32_F103X6.h
@@ -0,0 +1,276 @@
+/*
+ * STM32_F103X6.h
+ *
+ *  Created on: Dec 22, 2022
+ *      Author: aliab
+ */
+
+#ifndef INC_STM32_F103X6_H_
+#define INC_STM32_F103X6_H_
+
+//-----------------------------
+//Includes
+//-----------------------------
+#include <stdlib.h>
+#include <stdint.h>
+
+
+//-----------------------------
+//Base addresses for Memories
+//-----------------------------
+
+#define Flash_Memory_BASE 						0x08000000UL
+#define System_Memory_BASE						0x1FFFF000UL
+#define SRam_BASE								0x20000000UL
+
+#define Peripherals_BASE						0x40000000UL
+#define Cortex_m3_Internal_peripherals_BASE		0xE0000000UL
+
+//NVIC
+#define NVIC_BASE                                0xE000E100
+
+//NVIC REGISTERS MAP
+#define NVIC_ISER0                              *( volatile uint32_t *)(NVIC_BASE+0x0000)
+#define NVIC_ISER1                              *( volatile uint32_t *)(NVIC_BASE+0x0004)
+#define NVIC_ISER2                              *( volatile uint32_t *)(NVIC_BASE+0x0008)
+#define NVIC_ICER0                              *( volatile uint32_t *)(NVIC_BASE+0x0080)
+#define NVIC_ICER1                              *( volatile uint32_t *)(NVIC_BASE+0x0084)
+#define NVIC_ICER2                              *( volatile uint32_t *)(NVIC_BASE+0x0088)
+
+//-----------------------------
+//Base addresses for AHB Peripherals
+//-----------------------------
+
+#define RCC_BASE		0x40021000UL
+
+//*****
+
+///Base addresses for APB2 Peripherals
+
+//GPIO
+
+//GPIO A and GPIO B are totally included in LQFP48 package
+#define GPIOA_BASE		0x40010800UL
+#define GPIOB_BASE		0x40010C00UL
+
+//GPIO C and GPIO D are partially included in LQFP48 package
+#define GPIOC_BASE		0x40011000UL
+#define GPIOD_BASE		0x40011400UL
+
+//GPIO E are not included in LQFP48 package
+#define GPIOE_BASE		0x40011800UL
+
+//------
+//EXTI
+#define EXTI_BASE 								(Peripherals_BASE + 0x00010400UL)
+
+//AFIO
+#define AFIO_BASE 								(Peripherals_BASE + 0x00010000UL)
+
+//USART1
+#define USART1_BASE								0x40013800UL
+
+//-----------------------------
+//Base addresses for APB1 Peripherals
+//-----------------------------
+
+//USART2
+#define USART2_BASE								0x40004400UL
+
+//USART3
+#define USART3_BASE								0x40004800UL
+
+//=============================================================================================================
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Peripheral register:
+//-*-*-*-*-*-*-*-*-*-*-*
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Peripheral register GPIO:
+//-*-*-*-*-*-*-*-*-*-*-*
+
+typedef	struct{
+	volatile uint32_t CRL;
+	volatile uint32_t CRH;
+	volatile uint32_t IDR;
+	volatile uint32_t ODR;
+	volatile uint32_t BSRR;
+	volatile uint32_t BRR;
+	volatile uint32_t LCKR;
+}GPIO_TypeDef;
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Peripheral register RCC:
+//-*-*-*-*-*-*-*-*-*-*-*
+
+typedef struct{
+	volatile uint32_t CR;
+	volatile uint32_t CFGR;
+	volatile uint32_t CIR;
+	volatile uint32_t APB2RSTR;
+	volatile uint32_t APB1RSTR;
+	volatile uint32_t AHBENR;
+	volatile uint32_t APB2ENR;
+	volatile uint32_t APB1ENR;
+	volatile uint32_t BDCR;
+	volatile uint32_t CSR;
+}RCC_TypeDef;
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Peripheral register EXTI:
+//-*-*-*-*-*-*-*-*-*-*-*
+
+typedef struct{
+	volatile uint32_t IMR;
+	volatile uint32_t EMR;
+	volatile uint32_t RTSR;
+	volatile uint32_t FTSR;
+	volatile uint32_t SWIER;
+	volatile uint32_t PR;
+}EXTI_TypeDef;
+
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Peripheral register AFIO:
+//-*-*-*-*-*-*-*-*-*-*-*
+typedef struct{
+	volatile uint32_t EVCR;
+		volatile uint32_t MAPR;
+
+		/*volatile uint32_t EXTICR1;
+		volatile uint32_t EXTICR2;
+		volatile uint32_t EXTICR3;
+		volatile uint32_t EXTICR4; */  //0X14
+		volatile uint32_t EXTICR[4];
+
+		volatile uint32_t RESERVED0; //0X18
+		volatile uint32_t MAPR2;     //0X1C
+}AFIO_TypeDef;
+
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Peripheral Register: USART
+//-*-*-*-*-*-*-*-*-*-*-*-
+typedef struct{
+	volatile uint32_t SR;
+	volatile uint32_t DR;
+	volatile uint32_t BBR;
+	volatile uint32_t CR1;
+	volatile uint32_t CR2;
+	volatile uint32_t CR3;
+	volatile uint32_t GTPR;
+
+}USART_Typedef;
+
+
+
+//============================================================================================
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//Peripheral Instants:
+//-*-*-*-*-*-*-*-*-*-*-
+
+#define RCC 							 ((RCC_TypeDef *)RCC_BASE)
+
+
+#define GPIOA 							 ((GPIO_TypeDef *)GPIOA_BASE)
+#define GPIOB 							 ((GPIO_TypeDef *)GPIOB_BASE)
+#define GPIOC 							 ((GPIO_TypeDef *)GPIOC_BASE)
+#define GPIOD 							 ((GPIO_TypeDef *)GPIOD_BASE)
+#define GPIOE 							 ((GPIO_TypeDef *)GPIOE_BASE)
+
+
+#define EXTI 							 ((EXTI_TypeDef *)EXTI_BASE)
+
+#define AFIO 							 ((AFIO_TypeDef *)AFIO_BASE)
+
+#define USART1 										((USART_Typedef *) USART1_BASE)
+#define USART2 										((USART_Typedef *) USART2_BASE)
+#define USART3 										((USART_Typedef *) USART3_BASE)
+
+//=================================================================================================
+
+//-*-*-*-*-*-*-*-*-*-*-*-
+//clock enable Macros:
+//-*-*-*-*-*-*-*-*-*-*-*
+
+#define RCC_GPIOA_CLK_EN()				(RCC->APB2ENR |= (1<<2))
+#define RCC_GPIOB_CLK_EN()				(RCC->APB2ENR |= (1<<3))
+#define RCC_GPIOC_CLK_EN()				(RCC->APB2ENR |= (1<<4))
+#define RCC_GPIOD_CLK_EN()				(RCC->APB2ENR |= (1<<5))
+#define RCC_GPIOE_CLK_EN()				(RCC->APB2ENR |= (1<<6))
+
+#define RCC_AFIO_CLK_EN()				(RCC->APB2ENR |= (1<<0))
+
+//USART
+#define RCC_USART1_CLK_EN()							(RCC->APB2ENR |= (1 << 14))
+#define RCC_USART2_CLK_EN()							(RCC->APB1ENR |= (1 << 17))
+#define RCC_USART3_CLK_EN()							(RCC->APB1ENR |= (1 << 18))
+
+#define RCC_USART1_RESET()							(RCC->APB2RSTR |= (1 << 14))
+#define RCC_USART2_RESET()							(RCC->APB1RSTR |= (1 << 17))
+#define RCC_USART3_RESET()							(RCC->APB1RSTR |= (1 << 18))
+
+
+/*==========================================================*/
+/*EXTI_IRQ MACROS*/
+/*==========================================================*/
+#define EXTI0_IRQ                         6
+#define EXTI1_IRQ                         7
+#define EXTI2_IRQ                         8
+#define EXTI3_IRQ                         9
+#define EXTI4_IRQ                         10
+#define EXTI5_IRQ                         23
+#define EXTI6_IRQ                         23
+#define EXTI7_IRQ                         23
+#define EXTI8_IRQ                         23
+#define EXTI9_IRQ                         23
+#define EXTI10_IRQ                        40
+#define EXTI11_IRQ                        40
+#define EXTI12_IRQ                        40
+#define EXTI13_IRQ                        40
+#define EXTI14_IRQ                        40
+#define EXTI15_IRQ                        40
+
+//USART
+#define		USART1_IRQ			37
+#define		USART2_IRQ			38
+#define		USART3_IRQ			39
+/*==========================================================*/
+/*NVIC IRQ ENABLE/DISABLE MACROS*/
+/*==========================================================*/
+//ENABLE
+#define NVIC_IRQ6_EXTI0_ENABLE()             (NVIC_ISER0 |=(1<<6))
+#define NVIC_IRQ7_EXTI1_ENABLE()             (NVIC_ISER0 |=(1<<7))
+#define NVIC_IRQ8_EXTI2_ENABLE()             (NVIC_ISER0 |=(1<<8))
+#define NVIC_IRQ9_EXTI3_ENABLE()             (NVIC_ISER0 |=(1<<9))
+#define NVIC_IRQ10_EXTI4_ENABLE()            (NVIC_ISER0 |=(1<<10))
+#define NVIC_IRQ23_EXTI5_9_ENABLE()          (NVIC_ISER0 |=(1<<23))
+#define NVIC_IRQ40_EXTI0_15_ENABLE()         (NVIC_ISER1 |=(1<<8)) //40-32=8
+
+//DISABLE
+#define NVIC_IRQ6_EXTI0_DISABLE()             (NVIC_ICER0 &=~(1<<6))
+#define NVIC_IRQ7_EXTI1_DISABLE()             (NVIC_ICER0 &=~(1<<7))
+#define NVIC_IRQ8_EXTI2_DISABLE()             (NVIC_ICER0 &=~(1<<8))
+#define NVIC_IRQ9_EXTI3_DISABLE()             (NVIC_ICER0 &=~(1<<9))
+#define NVIC_IRQ10_EXTI4_DISABLE()            (NVIC_ICER0 &=~(1<<10))
+#define NVIC_IRQ23_EXTI5_9_DISABLE()          (NVIC_ICER0 &=~(1<<23))
+#define NVIC_IRQ40_EXTI0_15_DISABLE()         (NVIC_ICER1 &=~(1<<8)) //40-32=8
+
+
+//USART
+#define NVIC_IRQ37_USART1_ENABLE()					(NVIC_ISER1 |= (1 << (USART1_IRQ - 32)))
+#define NVIC_IRQ38_USART2_ENABLE()					(NVIC_ISER1 |= (1 << (USART2_IRQ - 32)))
+#define NVIC_IRQ39_USART3_ENABLE()					(NVIC_ISER1 |= (1 << (USART3_IRQ - 32)))
+
+#define NVIC_IRQ37_USART1_DISABLE()					(NVIC_ICER1 |= (1 << (USART1_IRQ - 32)))
+#define NVIC_IRQ38_USART2_DISABLE()					(NVIC_ICER1 |= (1 << (USART2_IRQ - 32)))
+#define NVIC_IRQ39_USART3_DISABLE()					(NVIC_ICER1 |= (1 << (USART3_IRQ - 32)))
+
+
+
+
+
+
+#endif /* INC_STM32_F103X6_H_ */
